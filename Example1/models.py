56c867da1486566904d8cd6c2dceab914742ab1f
JaimeGrimaldo <DankerGamer-@hotmail.com>
2020-07-04 05:52:26 -0500

    todo bien

--- Example1/models.py
+++ Example1/models.py
@@ -1,16 +1,15 @@
 from django.db import models
 
 
-
 class Example1(models.Model):
     name = models.CharField(max_length= 255, null = False)
     edad =models.IntegerField(null = False, default=1)
     direccion = models.CharField(max_length= 200, null = False, default = '')
-    curp = models.TextField(max_length= 16,null = False, default = '')
+    curp = models.CharField(max_length= 16,null = False, default = '')
 
 
     def __str__(self):
-        return self.name,self.edad
+        return self.name,self.edad,self.direccion,self.curp
     
     class Meta:
         db_table = 'Example1'
